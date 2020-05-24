11d0ef528d010f38ef081fe000395e4d4912d4b7
Daniela Flores <diflores@uc.cl>
2020-05-24 11:17:46 -0400

    Add a simple homepage

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="home-container">
+    <h1 id="site-name">Movie Reviews</h1>
+    <h2 id="site-slogan">Don’t lose your time with horrible movies, check our site first!</h2>
+  </div>
+</template>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Atma&family=Bangers&display=swap");
+#home-container {
+  background-color: #714dd2;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  overflow-x: hidden;
+  left: 0;
+}
+#site-name {
+  font-family: "Bangers";
+  font-size: 80pt;
+  color: #ffffff;
+  text-align: center;
+}
+#site-slogan {
+  font-family: "Atma";
+  font-size: 20pt;
+  color: #ffffff;
+  text-align: center;
+}
+</style>
