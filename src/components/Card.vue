674ed407ecdab58947d979dca7b191dece3cbd70
jatrijori <jtjorquera@uc.cl>
2020-05-28 23:23:21 -0400

    add card component; edit home view to show discover movies

---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card class="mx-auto" max-width="350">
+    <v-img
+      :src="cardImage"
+      height="180px"
+      contain
+    ></v-img>
+    <v-card-title id="card-title">
+      {{ cardTitle }}
+    </v-card-title>
+    <v-card-subtitle id="card-subtitle">
+      {{ cardSubtitle }}
+    </v-card-subtitle>
+  </v-card>
+</template>
+<script>
+export default {
+  props : {
+    cardTitle: {
+      type: String,
+      required: true
+    },
+    cardSubtitle: {
+      type: String,
+    },
+    cardImage: {
+      type: String,
+    },
+  },
+};
+</script>
+<style>
+#card-title {
+  color: var(--grey);
+  font-weight: 400;
+}
+#card-subtitle {
+  color: var(--grey);
+  font-weight: 300;
+}
+</style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -12,30 +12,57 @@
         append-icon="mdi-magnify"
       ></v-text-field>
     </div>
+    <div v-if="!results.length">
+      <h1 id="results-title">Today's featured movies</h1>
+
+      <h1 v-if="most_popular_movies.length" id="discover-title">Most popular movies</h1>
+      <div class="movies-container">
+        <Card
+          v-for="result in most_popular_movies" :key="result.id"
+          :card-title="result.title"
+          :card-subtitle="result.plot"
+          :card-image="result.posterURL"
+        ></Card>
+      </div>
+
+      <h1 v-if="most_popular_kids_movies.length" id="discover-title">Most popular kids movies</h1>
+      <div class="movies-container">
+        <Card
+          v-for="result in most_popular_kids_movies" :key="result.id"
+          :card-title="result.title"
+          :card-subtitle="result.plot"
+          :card-image="result.posterURL"
+        ></Card>
+      </div>
+
+      <h1 v-if="best_movies_2019.length" id="discover-title">Best movies from 2019</h1>
+      <div class="movies-container">
+        <Card
+          v-for="result in best_movies_2019" :key="result.id"
+          :card-title="result.title"
+          :card-subtitle="result.plot"
+          :card-image="result.posterURL"
+        ></Card>
+      </div>
+    </div>
+
     <h1 v-if="results.length" id="results-title">Movie Search Results</h1>
     <div class="movies-container">
-      <v-card
-        class="mx-auto"
-        max-width="350"
+      <Card
         v-for="result in results" :key="result.id"
-      >
-        <v-img
-          :src="result.posterURL"
-          height="200px"
-          contain
-        ></v-img>
-        <v-card-title id="card-title">
-          {{ result.title }}
-        </v-card-title>
-        <v-card-subtitle id="card-subtitle">
-          {{ result.plot }}
-        </v-card-subtitle>
-      </v-card>
+        :card-title="result.title"
+        :card-subtitle="result.plot"
+        :card-image="result.posterURL"
+      ></Card>
     </div>
   </section>
 </template>
 <script>
+import Card from "./Card";
 export default {
+  components: {
+    Card,
+  },
   computed: {
     isLoggedIn: function() {
       return this.$store.getters.isLoggedIn;
@@ -44,8 +71,76 @@ export default {
   data() {
     return {
       movie: "",
-      results: []
+      results: [],
+      best_movies_2019: [],
+      most_popular_movies: [],
+      most_popular_kids_movies: [],
     };
+  },
+  created() {
+    if (this.$store.getters.isLoggedIn) {
+      const posterBaseURL = "http://image.tmdb.org/t/p/w185";
+      this.$http({
+        url: `${process.env.VUE_APP_API_BASE_URL}/discover-movie`,
+        params: { sort_by: "popularity.desc" },
+        method: "GET"
+      }).then(response => {
+        this.most_popular_movies = [];
+        response.data.results.forEach(movie => {
+          if (movie.poster_path) {
+            const result = {};
+            result["posterURL"] = `${posterBaseURL}${movie.poster_path}`;
+            result["id"] = movie.id;
+            result["title"] = movie.title;
+            result["plot"] =
+              movie.overview.length > 140
+                ? movie.overview.substring(0, 140).trimRight() + "..."
+                : movie.overview;
+            this.most_popular_movies.push(result);
+          }
+        });
+      });
+      this.$http({
+        url: `${process.env.VUE_APP_API_BASE_URL}/discover-movie`,
+        params: { certification_country: "US", certification: "G", sort_by: "popularity.desc" },
+        method: "GET"
+      }).then(response => {
+        this.most_popular_kids_movies = [];
+        response.data.results.forEach(movie => {
+          if (movie.poster_path) {
+            const result = {};
+            result["posterURL"] = `${posterBaseURL}${movie.poster_path}`;
+            result["id"] = movie.id;
+            result["title"] = movie.title;
+            result["plot"] =
+              movie.overview.length > 140
+                ? movie.overview.substring(0, 140).trimRight() + "..."
+                : movie.overview;
+            this.most_popular_kids_movies.push(result);
+          }
+        });
+      });
+      this.$http({
+        url: `${process.env.VUE_APP_API_BASE_URL}/discover-movie`,
+        params: { year: 2019, sort_by: "vote_average.desc" },
+        method: "GET"
+      }).then(response => {
+        this.best_movies_2019 = [];
+        response.data.results.forEach(movie => {
+          if (movie.poster_path) {
+            const result = {};
+            result["posterURL"] = `${posterBaseURL}${movie.poster_path}`;
+            result["id"] = movie.id;
+            result["title"] = movie.title;
+            result["plot"] =
+              movie.overview.length > 140
+                ? movie.overview.substring(0, 140).trimRight() + "..."
+                : movie.overview;
+            this.best_movies_2019.push(result);
+          }
+        });
+      });
+    }
   },
   methods: {
     search: function() {
@@ -86,8 +181,13 @@ export default {
   flex-direction: row;
   width: 24em;
 }
+#discover-title {
+  font-weight: 400;
+  color: var(--secondary-color);
+  padding-top: 2em;
+}
 #results-title {
-  font-weight: 300;
+  font-weight: 400;
 }
 #card-title {
   color: var(--grey);
@@ -98,11 +198,10 @@ export default {
   font-weight: 300;
 }
 .movies-container {
-  margin-top: 2em;
-  padding-bottom: 3em;
+  margin: 2em 0 2em;
   display: grid;
   justify-items: center;
-  grid-template-columns: repeat(auto-fit, minmax(20em, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(18em, 1fr));
   grid-gap: 2em;
   width: 100%;
   height: 100%;
