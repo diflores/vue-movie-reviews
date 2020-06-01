cfb3bc6d960e8226e8c45f01c03e191af7d63f68
jatrijori <jtjorquera@uc.cl>
2020-05-31 22:55:46 -0400

    add profile view file

---
+++ src/views/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="profile-container" v-if="isLoggedIn" >
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    isLoggedIn: function() {
+      return this.$store.getters.isLoggedIn;
+    }
+  },
+};
+</script>
+<style>
+#profile-container {
+  height: 100%;
+  width: 100%;
+  color: var(--grey);
+  padding: 2em;
+}
+</style>
